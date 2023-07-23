797be4b7b477f88fffeeacfaf37490bc99c99d2e
koheinthurako <captainthura99@gmail.com>
2023-07-23 15:55:58 +0630

    Start note app

--- app.js
+++ app.js
@@ -1,99 +1,104 @@
 const showBtn = document.querySelector(".showBtn");
-const cardBox = document.querySelector(".cardBox");
-const closeBtn = document.querySelector(".closeBtn");
-const input = document.querySelector("input");
-const text = document.querySelector(".text");
+const formCard = document.querySelector(".formCard");
 const addBtn = document.querySelector(".addBtn");
-const noteBox = document.querySelector(".noteBox");
+const closeBtn = document.querySelector(".closeBtn");
+const card = document.querySelector(".card");
+const title = document.querySelector(".title");
+const text = document.querySelector(".text");
+const cardBox = document.querySelector(".container .cardBox");
+const confirmDelBtn = document.querySelector(".confirmDelBtn");
+
+let target;
 
 function showCard() {
-    cardBox.classList.remove("d-none");
-    cardBox.classList.add("d-block");
+    formCard.classList.remove("d-none");
+    formCard.classList.add("d-block");
 }
 
 function hideCard() {
-    cardBox.classList.remove("d-block");
-    cardBox.classList.add("d-none");
+    formCard.classList.remove("d-block");
+    formCard.classList.add("d-none");
 }
 
 function showNote() {
-    noteBox.classList.add("d-block");
-    noteBox.classList.remove("d-none")
+    card.classList.add("d-block");
+    card.classList.remove("d-none")
 }
 
 function hideNote() {
-    noteBox.classList.add("d-none");
-    noteBox.classList.remove("d-block")
+    card.classList.add("d-none");
+    card.classList.remove("d-block")
 }
 
-function noteBoxVisibility() {
-    let notes = noteBox.querySelectorAll(".note");
-    if(notes.length === 0) {
-        hideNote();
-    } else {
-        showNote();
-    }
-}
-
-function delNote() {
-    console.log("delNote");
-    if(window.confirm("Are you sure to delete this note?")){
-        this.parentElement.parentElement.remove();
-        noteBoxVisibility();
-    }
+function handleDelete() {
+    target.remove();
 }
 
 function createCard() {
-    let box = document.createElement("div");
-    let content = document.createElement("p");
-    let span = document.createElement("span");
-    let i = document.createElement("i");
+    let div = document.createElement("div");
+    div.classList.add("card", "inputCard", "text-start", "p-2", "border-0", "rounded-4", "overflow-auto", "m-lg-0");
+    div.style.height = "200px";
+    div.style.width = "300px";
 
-    box.classList.add("px-4", "py-2", "bg-primary", "rounded-3", "text-white", "note", "d-flex", "align-items-center", "justify-content-center", "gap-2");
+    div.innerHTML = `
+        <div class="card-body d-flex flex-column overflow-auto mb-3">
+            <h4 class="card-title mb-2 fw-bold">${title.value}</h4>
+            <p class="card-text small text-muted">${text.value}</p>
+        </div>
+        <div class="card-footer bg-white">
+            <button class="btn btn-sm btn-danger w-100 mt-1 delBtn" data-bs-toggle="modal" data-bs-target="#exampleModal">Delete</button>
+        </div>
+        `;
 
-    content.innerText = input.value;
-    content.classList.add("m-0", "ps-2");
+    cardBox.append(div);
 
-    i.classList.add("bi", "bi-x");
-    i.style.fontSize = "1.5rem";
+    let delBtn = document.querySelectorAll(".delBtn");
+    delBtn.forEach(btn => {
+        btn.addEventListener('click', e => {
+            target = e.target.parentElement.parentElement;
+            confirmDelBtn.addEventListener('click', handleDelete);
+        })
+        
+    });
 
-    span.append(i);
-
-    box.append(content);
-    box.append(span);
-    noteBox.append(box);
-
-    noteBox.classList.remove("d-none")
-    noteBox.classList.add("d-block")
-
-    input.value = null;
-
-    i.addEventListener('click', delNote);
-    
 }
 
 showBtn.addEventListener('click', _ => {
    showCard(); 
 });
 
+addBtn.addEventListener('click', _ => {
+    if(title.value || text.value) {
+        createCard();
+        hideCard();
+        title.value = null;
+        text.value = null;
+    }
+});
+
 closeBtn.addEventListener('click', _ => {
     hideCard();
-    input.value = null;
+    title.value = null;
 })
 
-input.addEventListener('keyup', e => {
+title.addEventListener('keyup', e => {
     if(e.key == "Enter") {
-        if(input.value) {
+        if(title.value) {
             createCard();
             hideCard();
+            title.value = null;
+            text.value = null;
         }
     }
 });
 
-addBtn.addEventListener('click', _ => {
-    if(input.value) {
-        createCard();
-        hideCard();
+text.addEventListener('keyup', e => {
+    if(e.key == "Enter") {
+        if(title.value) {
+            createCard();
+            hideCard();
+            title.value = null;
+            text.value = null;
+        }
     }
 });
-
